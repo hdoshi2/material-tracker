6890ee9c6aab43094e704580c1561b87601422c1
Hari.Doshi <hdoshi2@gmail.com>
2020-05-11 20:42:07 -0400

    ADD: Preliminary folder structure

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import '../App.css';
+import {Header, Footer} from './Layouts';
+import Exercises from './Exercises';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React TEST
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return(
+      <Fragment>
+        <Header />
+        <Exercises />
+        <Footer />
+      </Fragment>
+    )
+  }
 }
 
 export default App;
