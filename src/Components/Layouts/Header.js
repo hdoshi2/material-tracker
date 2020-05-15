f0443e74fb0171dcb422e8617e0dbe1a415f44c1
Hari.Doshi <hdoshi2@gmail.com>
2020-05-14 22:56:36 -0400

    ADD: MaterialUI, Header and Footer structure

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,8 +1,35 @@
 import React from 'react'
+// import { makeStyles } from '@material-ui/core/styles';
+import {AppBar, Toolbar, Typography, IconButton, Button} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default props =>
-<div>
-  <h1>
-    Header
-  </h1>
-</div>
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   menuButton: {
+//     marginRight: theme.spacing(2),
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+// }));
+
+export default (props) =>{
+  // const classes = useStyles();
+  return(
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" color="inherit" style={{flex: 1}}>
+        News
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Toolbar>
+  </AppBar>
+
+  )
+}
+
