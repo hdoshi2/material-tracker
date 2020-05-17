31fd65ebecd0f5c9ab199aca32f9e9ac782a830b
Hari.Doshi <hdoshi2@gmail.com>
2020-05-17 09:34:49 -0400

    ADD: function getExercisesByMuscles to sort through all exercises

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,11 +5,26 @@ import Exercises from './Exercises';
 import {muscles, exercises} from '../store.js'
 
 class App extends Component{
-  states = {
+  state = {
     exercises
   }
-  
+
+  getExercisesByMuscles(){
+    return Object.entries(
+      this.state.exercises.reduce((exercises, exercise) => {
+      const {muscles} = exercise;
+
+      exercises[muscles] = exercises[muscles]
+        ? [...exercises[muscles], muscles]
+        : [exercise]
+
+      return exercises
+    }, {})
+    )
+  }
+
   render(){
+    console.log(this.getExercisesByMuscles())
     return(
       <Fragment>
         <Header />
